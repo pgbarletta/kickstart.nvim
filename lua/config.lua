a212b590b7ceb4b423f520fdde67150c342ebb31
pgbarletta <pbarletta@gmail.com>
2025-07-18 13:50:52 -0400

    light theme is the default one

--- lua/config.lua
+++ lua/config.lua
@@ -8,7 +8,7 @@ require('catppuccin').setup {
   show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
   term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
   dim_inactive = {
-    enabled = false, -- dims the background color of inactive window
+    enabled = true, -- dims the background color of inactive window
     shade = 'dark',
     percentage = 0.15, -- percentage of the shade to apply to the inactive window
   },
@@ -47,7 +47,7 @@ require('catppuccin').setup {
   },
 }
 
-vim.cmd.colorscheme 'catppuccin-frappe'
+vim.cmd.colorscheme 'catppuccin-latte'
 
 local cmp = require 'cmp'
 cmp.setup {
