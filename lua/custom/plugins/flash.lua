63b4d49a30d1037205b5c71fa57cddda914736c1
pgbarletta <pbarletta@gmail.com>
2025-11-11 09:01:05 -0500

    Add flash plugin

---
+++ lua/custom/plugins/flash.lua
@@ -0,0 +1,62 @@
+-- Flash.nvim plugin for quick navigation
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    -- labels = "asdfghjklqwertyuiopzxcvbnm",
+    labels = "asdfghjkl;",
+    -- other options configure here
+    modes = {
+      search = {
+        enabled = false,
+      },
+      char = {
+        enabled = true,
+        keys = { "f", "F", "t", "T" },
+      },
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+  },
+}
