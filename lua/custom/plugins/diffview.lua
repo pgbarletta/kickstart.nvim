17567850eadbc63fc55ff1de2dd0815d92a501f6
pgbarletta <pbarletta@gmail.com>
2026-04-03 10:10:57 -0400

    update diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -5,6 +5,7 @@ return {
     { '<leader>gd', '<cmd>DiffviewOpen dev<cr>', desc = 'Diff vs dev' },
     { '<leader>gD', '<cmd>DiffviewOpen dev...HEAD<cr>', desc = 'Branch diff' },
     { '<leader>gs', '<cmd>DiffviewOpen --staged<cr>', desc = 'Staged diff' },
+    { '<leader>gw', '<cmd>DiffviewOpen HEAD<cr>', desc = 'Workdir vs HEAD' },
   },
   opts = {},
 }
